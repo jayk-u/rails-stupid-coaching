f1bc107539b81e854e1ad46841fc401ca04b3a49
jay cormier <jay.cormier@me.com>
2021-02-08 14:52:05 +0000

    coach running - needs some polish (on CSS mostly) later

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(question)
+    if question == 'I am going to work right now!'
+      return 'Great!'
+    elsif question.include?('?')
+      return 'Silly question, get dressed and go to work!'
+    else return "I don't care, get dressed and go to work!"
+    end
+  end
+end
